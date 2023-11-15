f736e688943ecdbc2d4f1ad1b70dcf0818674469
Harby <ingharby@gmail.com>
2023-11-14 21:41:23 -0500

    end points expuestos del crud

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { TaskService } from "./task.service";
+import { Task } from '@prisma/client';
 
-@Controller()
-export class TaskController { }
+@Controller('/tasks')
+export class TaskController {
+    constructor(private readonly taskService: TaskService) { }
+
+    @Get()
+    async getAllTasks() {
+        return this.taskService.getAllTasks()
+    }
+
+    @Get(':id')
+    async getTaskById(@Param('id') id: string) {
+        return this.taskService.getTaskById(Number(id))
+    }
+
+    @Post()
+    async createTask(@Body() data: Task) {
+        return this.taskService.createTask(data)
+    }
+
+    @Delete(':id')
+    async deleteTask(@Param('id') id: string) {
+        return this.taskService.deleteTask(Number(id))
+    }
+
+    @Put(':id')
+    async updateTask(@Body() data: Task, @Param('id') id: string) {
+        return this.taskService.updateTask(Number(id), data)
+    }
+
+
+}

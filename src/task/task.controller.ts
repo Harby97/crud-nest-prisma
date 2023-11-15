82f13be6430401b99054439a844e316b69e3275e
Harby <ingharby@gmail.com>
2023-11-15 08:35:07 -0500

    Manejo de errores en el crud

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { Body, Controller, Delete, Get, NotFoundException, Param, Post, Put } from "@nestjs/common";
 import { TaskService } from "./task.service";
 import { Task } from '@prisma/client';
 
@@ -8,28 +8,38 @@ export class TaskController {
 
     @Get()
     async getAllTasks() {
-        return this.taskService.getAllTasks()
+        return await this.taskService.getAllTasks()
     }
 
     @Get(':id')
     async getTaskById(@Param('id') id: string) {
-        return this.taskService.getTaskById(Number(id))
+        const taskFound = await this.taskService.getTaskById(Number(id))
+        if (!taskFound) throw new NotFoundException('Task does not exist')
+        return taskFound
     }
 
     @Post()
     async createTask(@Body() data: Task) {
-        return this.taskService.createTask(data)
+        return await this.taskService.createTask(data)
     }
 
     @Delete(':id')
     async deleteTask(@Param('id') id: string) {
-        return this.taskService.deleteTask(Number(id))
+        try {
+            return await this.taskService.deleteTask(Number(id))
+        }
+        catch (error) {
+            throw new NotFoundException('Task does not exists')
+        }
     }
 
     @Put(':id')
     async updateTask(@Body() data: Task, @Param('id') id: string) {
-        return this.taskService.updateTask(Number(id), data)
+        try {
+            return await this.taskService.updateTask(Number(id), data)
+        }
+        catch (error) {
+            throw new NotFoundException('Task does not exists')
+        }
     }
-
-
 }
